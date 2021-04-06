9e883cd4b3203f5beffc70b048973febcd7d39ae
Samuel <samuelnbacelar@hotmal.com>
2021-04-05 22:37:00 -0300

    adding env variables in project

--- denv/denv/settings.py
+++ denv/denv/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST") if os.getenv("DB_HOST") is not None else 'localhost'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +83,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': DB_HOST,
         'PORT': 5432,
     }
 }
