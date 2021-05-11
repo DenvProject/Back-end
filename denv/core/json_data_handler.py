4dac055529255baa87e707e02ab4631d4fbe1c84
Samuel <samuelnbacelar@hotmal.com>
2021-05-11 12:27:10 -0300

    comentando o arquivo jon_data_handler

--- denv/core/json_data_handler.py
+++ denv/core/json_data_handler.py
@@ -3,12 +3,22 @@ import os
 cur_path = os.path.dirname(__file__)
 
 def get_json_data(json_name : str) -> dict:
+    """
+    usando um nome de arquivo, essa função retorna os dados
+    lidos do arquivo que deve estar contido no diratório Data
+    :param json_name:
+    :return: dict
+    """
     new_path = os.path.join(cur_path, f'../data/{json_name}')
     with open(new_path) as json_data:
         json_data = json.load(json_data)
     return json_data
 
 def handle_gastos_publicos_json():
+    """
+    lida com a estrutura do json de gastos_públicos e retorna uma nova estrutura organizada.
+    :return: dict
+    """
     result = {}
     gastos_publicos = get_json_data('gastos_publicos.json')
     anos_meses = list(gastos_publicos.keys())
